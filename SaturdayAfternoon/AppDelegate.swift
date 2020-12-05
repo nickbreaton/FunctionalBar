5bb16b40cbac4e33f04ac711b1221aa101a20a03
Nick Breaton <nick@breaton.com>
2020-12-05 14:03:04 -0500

    Initial Commit

---
+++ SaturdayAfternoon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SaturdayAfternoon
+//
+//  Created by Nick Breaton on 12/5/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SaturdayAfternoon/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SaturdayAfternoon
+//
+//  Created by Nick Breaton on 12/5/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
